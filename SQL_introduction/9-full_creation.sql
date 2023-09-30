424e20f45afd9d325a06faf162673040db23c205
cvuakdgreat <cvuak19@gmail.com>
2023-09-30 21:12:25 +0100

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,14 @@
 -- creating a new table with values
-CREATE TABLE second_table( id INT, name VARCHAR(256),score INT)
-	VALUES(1, “John”, 10),(2, “Alex”, 3),(3, “Bob”, 14),(4, “George”, 8);
+CREATE TABLE IF NOT EXISTS second_table
+( 	
+	id INT, 
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table(id, name, score) 
+	VALUES
+		(1, 'John', 10),
+		(2, 'Alex', 3),
+		(3, 'Bob', 14),
+		(4, 'George', 8);
